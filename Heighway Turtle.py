72ba70a3b26a32f786d96829fdc5aa7452c2003c
Thomas Geoghegan <47263357+geoghets@users.noreply.github.com>
2020-06-07 09:47:22 -0500

    Made it all into one function

--- Heighway Turtle.py
+++ Heighway Turtle.py
@@ -2,30 +2,20 @@ import turtle
 import time
 import tkinter
 
-joe = turtle.Turtle()
+def heighway(step, len):
+    joe = turtle.Turtle()
+    joe.pensize(1)
+    joe.shape('turtle')
+    joe.speed(400)
 
-joe.pensize(1)
-joe.shape('turtle')
-joe.speed(400)
+    a = [1]
+    for i in range(step):
+        addon = [-x for x in a]
+        addon.reverse()
+        addon = [1] + addon
+        for i in addon:
+            joe.right(90*i), joe.forward(6)
+        a += addon
 
-a = [1]
-step = 11
-
-for i in range(step):
-    neg_a = [-x for x in a]
-    neg_a.reverse()
-    a.append(1)
-
-    a += neg_a
-
-
-print(a)
-
-def heighway(a,len):
-    for i in a:
-        if i == 1:joe.right(90), joe.forward(len)
-        if i == -1: joe.left(90), joe.forward(len)
-
-
-heighway(a,5)
+heighway(step=8,len=10)
 tkinter.mainloop()
